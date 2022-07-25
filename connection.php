16aa232e167eceeae8d67fd63ede07cc3b45be2f
ibrahim597 <ibrahim.sarkar597@gmail.com>
2022-07-25 06:29:49 +0600

    connection

--- connection.php
+++ connection.php
@@ -7,10 +7,5 @@
  $conn = new mysqli( $hostname, $username, $password, $dbname);
 if($conn->connect_error){
    die("connection failed: " . $conn->connect_error);
-
-  
-
 }
-
-
  ?>
--- list_of_category.php
+++ list_of_category.php
@@ -12,6 +12,29 @@ require('connection.php');
     <title>LIST category</title>
  </head>
  <body>
- 
+  <?php 
+  $sql = "SELECT* FROM category";
+  $query = $conn->query($sql);
+  echo "<table border='1'>
+  <tr>
+  <th>Category</th>
+  <th>Date</th>
+  <th>Action</th>
+  </tr>";
+
+  while($data = mysqli_fetch_assoc($query)){
+  $category_name = $data['category_name'];
+  $category_entrydate = $data['category_entrydate'];
+
+  echo "<tr>
+            <td>$category_name</td>
+            <td>$category_entrydate</td>
+            <td><a href='edit_category.php'>Edit</a></td>
+            <td><a href='delet_category.php'>Edit</a></td>
+
+  </tr>";
+  }
+  
+  ?>
  </body>
  </html>
